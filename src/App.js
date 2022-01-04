dc5396f361536e2b051bfb5a3721211a61708416
shahrukh131 <shahrukhakib@gmail.com>
2022-01-04 09:50:57 +0600

    route issue fixing

--- src/App.js
+++ src/App.js
@@ -1,32 +1,39 @@
 import Breadcrumbs from "./components/sections/Breadcrumbs";
 import SideMenu from "./components/sections/SideMenu";
 import HomePage from "./pages/HomePage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import NotFound from "./pages/NotFound";
 import Footer from "./components/sections/Footer";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/auth/Login";
+import AuthLayout from "./layouts/AuthLayout";
+import AppLayout from "./layouts/AppLayout";
 function App() {
+
+  const AuthLayoutRoute = ({ childrens }) => {
+    return (
+      {
+        childrens: <Navigate to="/login"/>
+      }
+    );
+  };
   return (
+      <div >
     <BrowserRouter>
-      <div
-        className="header-fixed header-tablet-and-mobile-fixed toolbar-enabled toolbar-fixed toolbar-tablet-and-mobile-fixed aside-enabled aside-fixed"
-        style={{
-          "--kt-toolbar-height": "55px",
-          "--kt-toolbar-height-tablet-and-mobile": "55px",
-        }}
-      >
-        <Breadcrumbs />
-        <SideMenu />
+     
         <Routes>
-         <Route path="/" element={< Login/>} />
           
+          
+          <Route path="/" element={<AuthLayoutRoute><AuthLayout/></AuthLayoutRoute>} />
+          
+          <Route path="/login" element={< Login/>} />
           <Route path="/dashboard" element={< Dashboard/>} />
           <Route path="*" element={<NotFound />} />
+         
         </Routes>
-        <Footer/>
-      </div>
+       
     </BrowserRouter>
+      </div>
   );
 }
 
---
+++ src/layouts/AuthLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AuthLayout = () => {
+    return (
+        <div class="d-flex flex-column flex-root">
+            
+        </div>
+    )
+}
+
+export default AuthLayout
