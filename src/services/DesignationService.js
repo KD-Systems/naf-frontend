3c789466bf12d3b842fc3e16fd28ffd43407d984
hmimeee <hmimeee@gmail.com>
2022-01-10 19:18:19 +0600

    Company components updated

--- src/services/DesignationService.js
+++ src/services/DesignationService.js
@@ -1,46 +1,35 @@
-import http from "../http-common"
-import React,{useState} from "react";
+import http from "../http-common";
+const getAll = async () => {
+  const res = await http.get("/designations");
 
-
-
-
-
-const  getAll = async()=>{
-    const res =  await http.get("/designations")
-
-    return res.data
-    
-            
+  return res.data;
 };
 
-const get = async(id) =>{
-    const res = await http.get(`/designations/${id}`)
-    return res.data
+const get = async (id) => {
+  const res = await http.get(`/designations/${id}`);
+  return res.data;
 };
 
-const create = async(data) =>{
-    return http.post("/designations",data)
+const create = async (data) => {
+  return http.post("/designations", data);
 };
 
-const update = async(id,data) =>{
-    
-    const res = await http.put(`/designations/${id}`,data)
-    return res.data
+const update = async (id, data) => {
+  const res = await http.put(`/designations/${id}`, data);
+  return res.data;
 };
 
-const remove = async(id) => {
-    const res = await http.delete(`/designations/${id}`)
-    return res.data
+const remove = async (id) => {
+  const res = await http.delete(`/designations/${id}`);
+  return res.data;
 };
 
 const DesignationService = {
-    getAll,
-    get,
-    create,
-    update,
-    remove,
-    
-    
-  };
+  getAll,
+  get,
+  create,
+  update,
+  remove,
+};
 
-  export default DesignationService;
+export default DesignationService;
