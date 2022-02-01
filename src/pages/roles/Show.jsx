67d8d8154a5dcb5091e33cabc00ce9a428750551
Shahrukh Akib <shahrukhakib@gmail.com>
2022-02-01 12:41:21 +0600

    role permission working

--- src/pages/roles/Show.jsx
+++ src/pages/roles/Show.jsx
@@ -1,19 +1,27 @@
-import React,{useEffect,useState} from "react";
-import {useNavigate,useParams} from "react-router-dom"
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import RoleService from "services/RoleService";
 const ShowPermission = () => {
   const navigate = useNavigate();
-  const {id} = useParams();
+  const { id } = useParams();
   const [role, setRole] = useState({});
-  const getRole = async () => {
+  const [permissions, setPermissions] = useState([]);
+  const getRole = async (id) => {
     setRole(await RoleService.get(id));
+  };
+
+  const getPermission = async () => {
+    setPermissions(await RoleService.getPermission());
   };
 
   useEffect(() => {
     if (id) {
-      getRole();
+      getRole(id);
+      getPermission();
     }
   }, [id]);
+
+  console.log("💥", permissions);
 
   return (
     <>
@@ -21,75 +29,72 @@ const ShowPermission = () => {
         <div id="kt_content_container" className="container-xxl">
           <div className="card card-flush">
             <div className="card-header pt-8">
-              <div className="card-title">
-                <h2>{role?.name}</h2>
-              </div>
+              <div className="card-title">{/* <h2>{role?.name}</h2> */}</div>
             </div>
 
             <div className="card-body">
               <form className="form" id="kt_file_manager_settings">
-                <div className="fv-row row mb-15">
-                  <div className="col-md-3">
-                    <label className="fs-6 fw-bold">Sharing Permissions</label>
-                  </div>
-
-                  <div className="col-md-9">
-
-                    <div className="d-flex">
-                      <div className="form-check form-switch form-check-custom form-check-solid me-10">
-                      <input
-                          className="form-check-input h-30px w-50px"
-                          name="public_write"
-                          type="checkbox"
-                          value=""
-                          id="public_write"
-                        />
-                        <label className="form-check-label" htmlFor="org_read">
-                          Read
-                        </label>
+                {permissions?.map((item) => (
+                  <>
+                    <div className="fv-row row mb-15">
+                      <div className="col-md-2">
+                        <label className="fs-6 fw-bold">{item?.name}</label>
                       </div>
 
-                      <div className="form-check form-switch form-check-custom form-check-solid me-10">
-                      <input
-                          className="form-check-input h-30px w-50px"
-                          name="public_write"
-                          type="checkbox"
-                          value=""
-                          id="public_write"
-                        />
-                        <label className="form-check-label" htmlFor="org_write">
-                          Write
-                        </label>
+                      <div className="col-md-10">
+                        <div className="d-flex">
+                          {item?.permissions?.map((permission)=>(
+   <div className="form-check form-switch form-check-custom form-check-solid me-10">
+   <input
+     className="form-check-input h-30px w-50px"
+     name="public_write"
+     type="checkbox"
+     value=""
+     id="public_write"
+   />
+   <label
+     className="form-check-label"
+     htmlFor="org_read"
+   >
+     {permission.name}
+   </label>
+ </div>
+                          ))}
+                       
+                        </div>
+
+                        <div className="separator mt-6 mb-5"></div>
                       </div>
                     </div>
 
-                    <div className="separator mt-6 mb-5"></div>
+                   
+                  </>
+                ))}
+                 <div className="row mt-12 ">
+                      <div className="col-md-9 offset-md-3">
+                        <button
+                          type="button"
+                          className="btn btn-light me-3"
+                          onClick={() => {
+                            navigate(-1);
+                          }}
+                        >
+                          Cancel
+                        </button>
 
-
-
-                
-                  </div>
-                </div>
-
-                <div className="row mt-12">
-                  <div className="col-md-9 offset-md-3">
-                    <button type="button" className="btn btn-light me-3" onClick={()=>{navigate(-1)}}>
-                      Cancel
-                    </button>
-
-                    <button
-                      type="button"
-                      className="btn btn-primary"
-                      id="kt_file_manager_settings_submit"
-                    >
-                      <span className="indicator-label">Save</span>
-                      <span className="indicator-progress">
-                        Please wait...
-                        <span className="spinner-border spinner-border-sm align-middle ms-2"></span>
-                      </span>
-                    </button>
-                  </div>
-                </div>
+                        <button
+                          type="button"
+                          className="btn btn-primary"
+                          id="kt_file_manager_settings_submit"
+                        >
+                          <span className="indicator-label">Save</span>
+                          <span className="indicator-progress">
+                            Please wait...
+                            <span className="spinner-border spinner-border-sm align-middle ms-2"></span>
+                          </span>
+                        </button>
+                      </div>
+                    </div>
               </form>
             </div>
           </div>
