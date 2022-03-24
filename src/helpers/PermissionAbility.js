398994105c79a372bfa18c7276840cc18d44a420
hmimeee <hmimeee@gmail.com>
2022-03-24 10:48:49 +0600

    Changed authentication systems

--- src/helpers/PermissionAbility.js
+++ src/helpers/PermissionAbility.js
@@ -1,21 +1,18 @@
-import {
-  useSelector
-} from "react-redux";
-
 const PermissionAbility = ({
   children,
   permission
 }) => {
-  const permissions = useSelector((state) => state?.auth?.user?.permissions)
-  const role = useSelector((state) => state?.auth?.user?.role)
-  if (role === 'Admin') {
-    return children;
-  } else {
-    if (!permission)
-      return children;
+  let data = JSON.parse(localStorage.getItem('user'));
+  let user = data.user;
+  let permissions = user?.permissions;
 
-    return permissions?.includes(permission) ? children : null;
-  }
+  if (user?.role === 'Admin')
+    return children;
+
+  if (!permission)
+    return children;
+
+  return permissions?.includes(permission)?children : null;
 };
 
 export default PermissionAbility;
--- src/pages/layouts/AppLayout.jsx
+++ src/pages/layouts/AppLayout.jsx
@@ -1,11 +1,12 @@
-import Footer from "../../components/sections/Footer";
-import SideMenu from "../../components/sections/SideMenu";
+import Footer from "components/sections/Footer";
+import SideMenu from "components/sections/SideMenu";
 import { Outlet } from "react-router-dom";
-import NavBar from "../../components/sections/NavBar";
-import {  useSelector } from "react-redux";
+import NavBar from "components/sections/NavBar";
 import SideMenuCompany from "components/sections/SideMenuCompany";
+
 export default function AppLayout() {
-  const { user } = useSelector((state) => state.auth);
+  let data = JSON.parse(localStorage.getItem('user'))
+  let user = data.user
 
   //Replace the classes of body for the panel
   let body = document.getElementsByTagName("body");
@@ -16,15 +17,15 @@ export default function AppLayout() {
 
   return (
     <div className="page d-flex flex-row flex-column-fluid">
-      {user?.details!==null ? <SideMenuCompany/> : <SideMenu />}
-      
+      {user?.details !== null ? <SideMenuCompany /> : <SideMenu />}
+
       <div
         className="wrapper d-flex flex-column flex-row-fluid "
         id="wrapper"
       >
         <NavBar />
         <div className="post d-flex flex-column-fluid mt-5" id="kt_post">
-        <Outlet />
+          <Outlet />
         </div>
         <Footer />
       </div>
