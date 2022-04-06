7103914742bba998be29cfc620d0e55c6baa5f3e
unknown <shohorabshanto@gmail.com>
2022-04-06 16:29:09 +0600

    new1

--- src/http-common.js
+++ src/http-common.js
@@ -9,7 +9,7 @@ toast.configure();
 let Api = axios.create({
   // baseURL: `//naf-api.viserx.net/api/`,
   // baseURL: `//api.naf-inventory.test/api/`,
-  baseURL: `//naf-inventory.test/api/`,
+  baseURL: `//naf-inventory-back.test/api/`,
   headers: {
     "Content-type": "application/json",
     "accept": "application/json",
