d62e54ca71eb89f1b56fb881393c5ea40e24fe4c
Imrul Afnan <imrul.afnan18@gmail.com>
2022-09-05 11:43:09 +0600

    Update Statstics in Dashboard

--- src/components/dashboard/StatisticsChartWidget.jsx
+++ src/components/dashboard/StatisticsChartWidget.jsx
@@ -80,14 +80,14 @@ const StatisticsChartWidget = ({ title, stats, trend, colors }: StatisticsChartW
                         <h3 className="mb-0">{stats}</h3>
                     </div>
                     <div className="align-self-center flex-shrink-0">
-                        <Chart
+                        {/* <Chart
                             className="apex-charts"
                             options={options}
                             series={series}
                             type="area"
                             height={45}
                             width={80}
-                        />
+                        /> */}
                         <span className={classNames('fw-bold', 'fs-13', trend.textClass)}>
                             <i className={trend.icon}></i> {trend.value}
                         </span>
