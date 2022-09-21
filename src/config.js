bc9a1005a1cc3c628e08acc56f7088fab556bcdb
unknown <shohorabshanto@gmail.com>
2022-09-21 10:50:40 +0600

    merge with main

--- src/config.js
+++ src/config.js
@@ -1,11 +1,7 @@
 export default {
   // baseUrl: "https://naf-api.viserx.net/api",
   // "baseUrl": 'http://nafappapi.viserx.net/api',
-<<<<<<< HEAD
-  baseUrl: "http://naf-inventory-back.test/api",
-=======
   baseUrl: "http://naf-inventory-back.test/api", 
->>>>>>> main
 }; 
 
 
